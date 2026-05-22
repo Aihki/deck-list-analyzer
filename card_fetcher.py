c08c9239d637682d9c28e0784a9c86d0ff96c2c4
aihki <laine.nestori.julius@gmail.com>
2026-05-22 16:45:48 +0300

    feat: starting card_fetcher and utils

---
+++ card_fetcher.py
@@ -0,0 +1,13 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+scryfall_api_url = os.getenv('scryfall-api')
+
+def search_card(card_name):
+    return
+
+def get_card_colors(card_name):
+    return
